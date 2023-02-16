107451ce213422d43e32b414e60f9bc9fa50881b
Abdul Razak Kareem <razakkareem12345@gmail.com>
2023-02-16 10:39:26 +0000

    This is my first c file

---
+++ my_first_c_program.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+    // Types of variables
+    int num = 45;  // Integer variable
+    float f = 3.423; // float variable
+    char c = 'c'; // character variable
+    
+    // Comments are also in C
+    /*
+    These are also multi comments
+    I can write multiple comments in these sections 
+    */
+    printf("Hello world");
+    printf("This is an integer %d\n",num);
+    printf("This is a float %f\n",f);
+    printf("This is a character %c\n",c);
+    printf("Hello, I am learning C on YouTube");
+    return 0;
+}
+
+// Library functions
+/* %d for integer
+%f for float
+%c for character*/
